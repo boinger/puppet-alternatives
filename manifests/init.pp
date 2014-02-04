feb97aa2c6db01ed465b2b5ac04e7591f62312e1
jv <jeff@jeffvier.com>
2014-02-04 15:23:44 -0800

    full paths

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 class alternatives {
   case $operatingsystem {
-    Scientific,centos,fedora,redhat: { $bin = 'alternatives' }
-    debian,ubuntu: { $bin = 'update-alternatives' }
+    Scientific,centos,fedora,redhat: { $bin = '/usr/sbin/alternatives' }
+    debian,ubuntu: { $bin = '/usr/sbin/update-alternatives' }
   }
 }
