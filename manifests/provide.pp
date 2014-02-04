6cb002e1d5dbf8e33e910d315c19efdd976958f4
jv <jeff@jeffvier.com>
2014-02-04 15:28:20 -0800

    forgot one var

--- manifests/provide.pp
+++ manifests/provide.pp
@@ -4,7 +4,7 @@ define alternatives::provide(
   $priority
 ){
   include ::alternatives
-  exec { "provide_alternative_${alternative_name}_${name}":
+  exec { "provide_alternative_${short_path}_${name}":
     command => "${alternatives::bin} --install ${short_path} ${name} ${target_path} ${priority}",
     unless => "${alternatives::bin} --display ${short_path} | grep -q '${target_path} - priority ${priority}'"
   }
